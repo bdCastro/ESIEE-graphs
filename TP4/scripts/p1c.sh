fd8375eee3184041e62bbd1fcde1916073318dd0
Bruno Castro <castrobruno98@gmail.com>
2023-03-09 10:31:54 +0000

    1c done 😳

---
+++ TP4/scripts/p1c.sh
@@ -0,0 +1,8 @@
+masks=/workspaces/MorphoGraph/PINK/Pinktmp/masks
+images=/workspaces/MorphoGraph/TP4/PS4-Images
+results=/workspaces/MorphoGraph/TP4/result_images
+
+erosion $images/cell.pgm $masks/carre5.pgm $results/$1
+dilation $results/$1 $masks/carre5.pgm $results/$1
+dilation $results/$1 $masks/carre5.pgm $results/$1
+erosion $results/$1 $masks/carre5.pgm $results/$1
