9f1ca4fdcd3a98f74207a990de26d54c187c76bc
Bruno Castro <castrobruno98@gmail.com>
2023-03-16 08:05:34 +0000

    start of p2 implementation ⚠️

---
+++ TP4/scripts/p2.sh
@@ -0,0 +1,5 @@
+masks=/workspaces/MorphoGraph/PINK/Pinktmp/masks
+images=/workspaces/MorphoGraph/TP4/PS4-Images
+results=/workspaces/MorphoGraph/TP4/result_images
+
+closeball $images/cell3.pgm 5 $results/$1
