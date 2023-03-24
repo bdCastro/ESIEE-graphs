c641604e2ac69a359df05b7e4b12cb258832f94c
Bruno Castro <castrobruno98@gmail.com>
2023-03-24 07:45:18 +0000

    doing PS5 p2 😧

---
+++ TP5/scripts/p2a.sh
@@ -0,0 +1,8 @@
+masks=/workspaces/MorphoGraph/PINK/Pinktmp/masks
+images=/workspaces/MorphoGraph/TP5/PS5-Images
+results=/workspaces/MorphoGraph/TP5/result_images
+tmp=/workspaces/MorphoGraph/TP5/tmp
+
+mkdir -p $tmp
+seuil $images/cil.pgm 110 $results/$1
+rm -d
